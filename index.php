c8a9ef3f0b514ca8bbf80f9baada1a598172029f
Sixeye <srinath@srinath.fr>
2018-12-03 13:36:52 +0100

    Primary access

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// Current page definition
+if (isset($_GET['page']) AND !empty($_GET['page'])){
+    $page = trim(strtolower($_GET['page']));
+} else {
+    $page ='home';
+}
+
+// Array with all pages
+$allPages = scandir('controllers/');
+
+if (in_array($page.'_controller.php', $allPages)){
+
+    // Include the page
+    include_once 'models/'.$page.'_model.php';
+    include_once 'controllers/'.$page.'_controller.php';
+    include_once 'views/'.$page.'_view.php';
+
+}; else {
+    echo 'Erreur 404';
+}
+
+?>
