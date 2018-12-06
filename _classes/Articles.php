6c991c62b041a6ac5f33d969bbef06a012ba151a
Sixeye <srinath@srinath.fr>
2018-12-06 11:36:56 +0100

    Classes handled

--- _classes/Articles.php
+++ _classes/Articles.php
@@ -2,6 +2,65 @@
 
 class Articles{
     
+    public $id;
+    public $title;
+    public $sentence;
+    public $content;
+    public $date;
+    public $author;
+    public $category;
+
+
+/**
+* Articles constructor.
+* @param $id
+*/
+
+    function __construct($id){
+        global $db;
+
+        $id = str_secur($id);
+
+        $reqArticle = $db->prepare('
+            SELECT a.*, au.firstname, au.lastname, c.name AS category
+            FROM articles a
+            INNER JOIN authors au ON au.id = a.author_id
+            INNER JOIN categories c ON c.id = a.category_id
+            WHERE a.id = ?
+            ');
+        $reqArticle->execute([$id]);
+        $data = $reqArticle->fetch();
+
+        $this->id = $id;
+        $this->title = $data['title'];
+        $this->content = $data['content'];
+        $this->sentence = $data['date'];
+        $this->date = $data['date'];
+        $this->author = $data['firstname'] . ' ' . $data['lastname'];
+        $this->category = $data['category'];
+
+    }
+
+/**
+* Sends all articles.
+* @return array
+*/
+
+    static function getAllArticles(){
+         global $db;
+
+        $id = str_secur($id);
+
+        $reqArticle = $db->prepare('
+            SELECT a.*, au.firstname, au.lastname, c.name AS category
+            FROM articles a
+            INNER JOIN authors au ON au.id = a.author_id
+            INNER JOIN categories c ON c.id = a.category_id
+            ');
+        $reqArticles->execute([]);
+        $data = $reqArticles->fetchAll();
+    }
+
 }
 
 ?>
---
+++ _classes/Authors.php
@@ -0,0 +1,43 @@
+<?php
+
+class Authors{
+
+public $id;
+public $firstname;
+public $lastname;
+
+/**
+* Authors constructor.
+* @param $id
+*/
+function __construct($id){
+    global $db;
+
+    $id = str_secur($id);
+
+    $reqAuthor = $db->prepare('SELECT * FROM authors WHERE id = ?');
+    $reqAuthor->execute([$id]);
+    $data = $reqAuthor->fetch();
+
+    $this->id = $id;
+    $this->firstname = $data['firstname'];
+    $this->lastname = $data['lastname'];
+}
+
+/**
+* Send all authors
+* @return array
+*/
+static function getAllAuthors(){
+
+    global $db;
+
+    $reqAuthors = $db->prepare('SELECT * FROM authors');
+    $reqAuthors->execute([]);
+    return $reqAuthors->fetchAll();
+
+    }
+
+}
+
+?>
