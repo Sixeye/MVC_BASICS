11e878a3574a07d2a71853841575e2b26cf9e331
Sixeye <srinath@srinath.fr>
2018-12-13 10:32:59 +0100

    README file has a todo list

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MVC_BASICS
+
+to do list
+-contact page to conceive
+-comments on each bill
+-create a moderation system for the comments
+-create an admin page
+-create an admin wysiwyg for the author
+-create an admin wysiwyg for the agent
+-check the indentation and comment when necessary
+-Put the project online
+-Check the routes, if everything is linked correctly
+-check the w3c validator
+-The project is done
--- views/includes/nav.php
+++ views/includes/nav.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="/index.html" data-bs-parallax-bg="true">JEAN FORTEROCHE</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+        <div class="container"><a class="navbar-brand" href="home" data-bs-parallax-bg="true">JEAN FORTEROCHE</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div
                 class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php?page=home" data-bs-hover-animate="pulse">ACCUEIL</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php?page=articles" data-bs-hover-animate="pulse">CHAPITRES</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php?page=actualites" data-bs-hover-animate="pulse">A PROPOS</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php?page=contact" data-bs-hover-animate="pulse">CONTACTEZ-MOI</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="home" data-bs-hover-animate="pulse">ACCUEIL</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="articles" data-bs-hover-animate="pulse">CHAPITRES</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="actualites" data-bs-hover-animate="pulse">A PROPOS</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="contact" data-bs-hover-animate="pulse">CONTACTEZ-MOI</a></li>
                 </ul>
             </div>
         </div>
