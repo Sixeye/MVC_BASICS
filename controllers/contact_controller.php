6ae397eff54503ac87a9e6335f2bd9e1dbcc118c
Sixeye <34404226+Sixeye@users.noreply.github.com>
2019-02-11 22:05:29 +0100

    Delete contact_controller.php

--- controllers/contact_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once 'models/contact_model.php';
-
-$msg_to_user    = Messages::msg_to_user();
-$createMessages = Messages::createMessages();
-
-include_once 'views/contact_view.php';
