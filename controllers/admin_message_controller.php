e86346b11f62e96c7513735e0f1d147704005797
Sixeye <34404226+Sixeye@users.noreply.github.com>
2019-02-11 22:05:57 +0100

    Delete admin_message_controller.php

--- controllers/admin_message_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once 'models/admin_message_model.php';
-
-$verify_session = Session::verify_session();
-$getAllMessages = Messages::getAllMessages();
-$deleteMessage  = Messages::deleteMessage();
-
-include_once 'views/admin_message_view.php';
