1e070a3d186e8b49725c244b19115bccb88e0a30
Sixeye <34404226+Sixeye@users.noreply.github.com>
2019-02-11 22:05:02 +0100

    Delete admin_nupdate_controller.php

--- controllers/admin_nupdate_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'models/admin_nupdate_model.php';
-
-$verify_session   = Session::verify_session();
-$allActualites    = Actualites::getAllActualites();
-$fillActualites   = Actualites::fillActualites();
-$updateActualites = Actualites::updateActualites();
-
-include_once 'views/admin_nupdate_view.php';
